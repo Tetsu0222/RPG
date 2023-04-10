bdeccb5147152fe75a988ed434a78e4b2bfd3ae1
tetsu <root2120@gmail.com>
2023-04-10 18:42:37 +0900

    気絶時も自動スキルが発動されないように修正

--- RPG3/src/main/java/com/example/rpg2/process/IsEndSkillStop.java
+++ RPG3/src/main/java/com/example/rpg2/process/IsEndSkillStop.java
@@ -9,6 +9,7 @@ public class IsEndSkillStop {
 		//行動不能系の状態異常が追加された場合、filterで判定を追加していく。
 		Long juds = allyData.getStatusSet().stream()
 			.filter( s -> s.getName().equals( "睡眠" ))
+			.filter( s -> s.getName().equals( "気絶" ))
 			.count();
 		
 		//行動不能系の状態異常が存在すれば、スタートスキルの発動を停止(falseで返す)
