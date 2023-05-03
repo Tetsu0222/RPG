1a7ad114c8f8c0e62b8ae3a7c910ab5467cefe9c
tetsu <root2120@gmail.com>
2023-05-03 13:42:10 +0900

    リファクタリング

--- RPG3/src/main/java/com/example/rpg2/action/skill/BuffSkill.java
+++ RPG3/src/main/java/com/example/rpg2/action/skill/BuffSkill.java
@@ -1,7 +1,6 @@
 package com.example.rpg2.action.skill;
 
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import com.example.rpg2.action.TargetAllyAction;
 import com.example.rpg2.battle.AllyData;
@@ -10,6 +9,7 @@ import com.example.rpg2.status.HolyBarrier;
 import com.example.rpg2.status.Hubaha;
 import com.example.rpg2.status.MagicBarrier;
 import com.example.rpg2.status.Normal;
+import com.example.rpg2.status.Poison;
 import com.example.rpg2.status.Status;
 
 import lombok.Data;
@@ -48,110 +48,138 @@ public class BuffSkill implements TargetAllyAction{
 	//味方への補助魔法
 		public AllyData action( AllyData receptionAllyData ) {
 			
-			//Random random = new Random(); どこかで使うかも
+			double buffPoint = 0.0;
+			Set<Status> statusSet = null;
 			
-			//防御補助魔法の処理
-			if( skill.getBuffcategory().equals( "DEF" )) {
+			switch( skill.getBuffcategory() ) {
+			
+				//守備力のバフ
+				case "DEF":
 				
-				double def = receptionAllyData.getCurrentDEF();
-				
-				//上昇上限チェック
-				if( def >= receptionAllyData.getDefaultDEF() * 2 ) {
-					this.resultMessage = receptionAllyData.getName() + "は、これ以上は守備力が上がらなかった･･･";
-				
-				//上限未達
-				}else{
-					double buffPoint = skill.getPercentage();
-					def = def * buffPoint;
+					double def = receptionAllyData.getCurrentDEF();
 					
+					//上昇上限チェック
+					if( def >= receptionAllyData.getDefaultDEF() * 2 ) {
+						this.resultMessage = receptionAllyData.getName() + "は、これ以上は守備力が上がらなかった･･･";
+						break;
+					}
+					
+					//上限未達
+					buffPoint = skill.getPercentage();
+					def = def * buffPoint;
+						
 					//補正値が上限を上回らないように再分岐
 					if( def > receptionAllyData.getDefaultDEF() * 2 ) {
 						def = receptionAllyData.getDefaultDEF() * 2 ;
 					}
+					
+					//キャラクターへ反映
 					receptionAllyData.setCurrentDEF( (int) def );
+					
+					//メッセージを設定
 					this.resultMessage = receptionAllyData.getName() + "の守備力が上がった!!";
-				}
-			
-				
-			//攻撃補助魔法
-			}else if( skill.getBuffcategory().equals( "ATK" )) {
-				
-				double atk = receptionAllyData.getCurrentATK();
-				
-				//上昇上限チェック
-				if( atk > receptionAllyData.getDefaultATK() * 2 ) {
-					this.resultMessage = receptionAllyData.getName() + "は、これ以上は攻撃力が上がらなかった･･･";
 					
-				//上限未達
-				}else{
-					double buffPoint = skill.getPercentage();
+					break;
+				
+				//攻撃力のバフ
+				case "ATK":
+					double atk = receptionAllyData.getCurrentATK();
+					
+					//上昇上限チェック
+					if( atk > receptionAllyData.getDefaultATK() * 2 ) {
+						this.resultMessage = receptionAllyData.getName() + "は、これ以上は攻撃力が上がらなかった･･･";
+						break;
+					}
+					//上限未達
+					buffPoint = skill.getPercentage();
 					atk = atk * buffPoint;
-					
+						
 					//補正値が上限を上回らないように再分岐
 					if( atk > receptionAllyData.getDefaultATK() * 2 ) {
 						atk = receptionAllyData.getDefaultATK() * 2;
 					}
-					receptionAllyData.setCurrentDEF( (int) atk );	
+					
+					//キャラクターへ反映
+					receptionAllyData.setCurrentDEF( (int) atk );
+					
+					//メッセージを設定
 					this.resultMessage = receptionAllyData.getName() + "の攻撃力が大きく上がった!!";
-				}
-			
-			//聖なる守り
-			}else if( skill.getBuffcategory().equals( "holybarrier" ) ) {
-				Set<Status> statusSet = this.goodStatus( receptionAllyData );
-				statusSet.remove( new HolyBarrier() );
-				statusSet.add( new HolyBarrier( receptionAllyData ) );
-				receptionAllyData.setStatusSet( statusSet );
-				receptionAllyData.setSurvival( 2 );
-				this.resultMessage = receptionAllyData.getName() + "は聖なる守りに包まれる。";
-				
-			//マジックバリア
-			}else if( skill.getBuffcategory().equals( "magicbarrier" ) ) {
-				Set<Status> statusSet = this.goodStatus( receptionAllyData );
-				statusSet.remove( new MagicBarrier() );
-				statusSet.add( new MagicBarrier( receptionAllyData ) );
-				receptionAllyData.setStatusSet( statusSet );
-				this.resultMessage = receptionAllyData.getName() + "は魔法に強くなった!!";
-				
-			//フバーハ
-			}else if( skill.getBuffcategory().equals( "fubaha" ) ) {
-				Set<Status> statusSet = this.goodStatus( receptionAllyData );
-				statusSet.remove( new Hubaha() );
-				statusSet.add( new Hubaha( receptionAllyData ) );
-				receptionAllyData.setStatusSet( statusSet );
-				this.resultMessage = receptionAllyData.getName() + "を優しい衣が包み込む!!";		
-				
-			//毒を治す魔法
-			}else if( skill.getBuffcategory().equals( "poison" ) ) {
-				
-				//対象キャラクターの状態異常を取得
-				Set<Status> statusSet = receptionAllyData.getStatusSet();
-				
-				//毒状態かチェック
-				Long sts = statusSet.stream()
-						.filter( s -> s.getName().equals( "毒" ))
-						.count();
-				int size = statusSet.size();
-				
-				//状態異常が毒のみ
-				if( sts == 1 && size == 1 ) {
-					statusSet.clear();
-					statusSet.add( new Normal() );
-					this.resultMessage = receptionAllyData.getName() + "の毒が治った!!";
 					
-				//状態異常が毒以外にもある。
-				}else if( sts == 1 && size > 1 ) {
-					statusSet = allyData.getStatusSet()
-							.stream()
-							.filter( s -> !s.getName().equals( "毒" ) )
-							.collect( Collectors.toSet() );
-					this.resultMessage = receptionAllyData.getName() + "の毒が治った!!";
-					
-				//毒状態ではない。
-				}else{
-					this.resultMessage = receptionAllyData.getName() + "に効果はなかった…";
-				}
+					break;
 				
-				receptionAllyData.setStatusSet( statusSet );
+				//聖なる守りを付与
+				case "holybarrier":
+					
+					//対象者の状態を取得
+					statusSet = this.goodStatus( receptionAllyData );
+					
+					//聖なる守りを上書きで付与
+					statusSet.remove( new HolyBarrier() );
+					statusSet.add( new HolyBarrier( receptionAllyData ) );
+					
+					//キャラクターへ反映
+					receptionAllyData.setStatusSet( statusSet );
+					receptionAllyData.setSurvival( 2 );
+					
+					//メッセージを設定
+					this.resultMessage = receptionAllyData.getName() + "は聖なる守りに包まれる。";
+					
+					break;
+				
+					
+				//マジックバリアを付与
+				case "magicbarrier":
+					statusSet = this.goodStatus( receptionAllyData );
+					statusSet.remove( new MagicBarrier() );
+					statusSet.add( new MagicBarrier( receptionAllyData ) );
+					receptionAllyData.setStatusSet( statusSet );
+					this.resultMessage = receptionAllyData.getName() + "は魔法に強くなった!!";
+					
+					break;
+				
+					
+				//フバーハを付与
+				case "fubaha":
+					statusSet = this.goodStatus( receptionAllyData );
+					statusSet.remove( new Hubaha() );
+					statusSet.add( new Hubaha( receptionAllyData ) );
+					receptionAllyData.setStatusSet( statusSet );
+					this.resultMessage = receptionAllyData.getName() + "を優しい衣が包み込む!!";
+					
+					break;
+					
+				
+				//毒を治す特技
+				case "poison":
+					
+					//対象キャラクターの状態異常を取得
+					statusSet = receptionAllyData.getStatusSet();
+					
+					//毒状態かチェック
+					if( !statusSet.contains( new Poison() )) {
+						this.resultMessage = receptionAllyData.getName() + "に効果はなかった…";
+						break;
+					}
+					
+					//毒以外にも状態異常を持っているかチェック
+					Long sts = statusSet.stream()
+							.filter( s -> s.getName().equals( "毒" ))
+							.count();
+					
+					int size = statusSet.size();
+					
+					//状態異常が毒のみ
+					if( sts == 1 && size == 1 ) {
+						statusSet.clear();
+						statusSet.add( new Normal() );
+						
+					//状態異常が毒以外にもある。
+					}else if( sts == 1 && size > 1 ) {
+						statusSet.remove( new Poison() );
+					}
+					
+					receptionAllyData.setStatusSet( statusSet );
+					this.resultMessage = receptionAllyData.getName() + "の毒が治った!!";
 			}
 			
 			return receptionAllyData;
@@ -161,10 +189,8 @@ public class BuffSkill implements TargetAllyAction{
 		//正常からへバフ状態（期限付き）へ変化
 		public Set<Status> goodStatus( AllyData receptionAllyData ){
 			
-			Set<Status> statusSet = receptionAllyData.getStatusSet()
-					.stream()
-					.filter( s -> !s.getName().equals( "正常" ) )
-					.collect( Collectors.toSet() );
+			Set<Status> statusSet = receptionAllyData.getStatusSet();
+			statusSet.remove( new Normal() );
 			
 			return statusSet;
 		}
